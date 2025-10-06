598f838f6582daa55cae61b0f127aff68847b6d3
gwang1224 <gracewang187@gmail.com>
2025-10-06 12:19:33 -0400

    model eval start

---
+++ model_eval.py
@@ -0,0 +1,18 @@
+"""
+Model evaluation script
+
+Purpose
+- Determine which large language model is best for generating fast payment sequences
+- Generates statistics based on
+    (1) # of valid sequences
+    (2)
+
+Current capabilities
+
+Usage
+
+Outputs (example counts from "David Nathan")
+   
+Notes
+
+"""
