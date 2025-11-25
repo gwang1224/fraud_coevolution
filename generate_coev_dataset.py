1369275337e077979b8e4c1c6ded3f174022e928
gwang1224 <gracewang187@gmail.com>
2025-11-25 09:58:17 -0500

    coevolution

--- generate_coev_dataset.py
+++ generate_coev_dataset.py
@@ -9,9 +9,9 @@ env = env_generator.create_environment()
 planner = llmplanner.LLMPlanner(env)
 
 
-def generate_sequences(env, planner, data_len=100, num_fraud_seq=50):
+def generate_sequences(env, planner, data_len=17, num_fraud_seq=8):
     fraud_ind = random.sample(range(0, data_len), num_fraud_seq)
-    with open("coev_seq_v2.json", "w") as f:
+    with open("coev_seq_v2.json", "a") as f:
         for i in range(data_len):
             print(f"Sequence {i} ---------------------------------------------")
             label = "fraud" if i in fraud_ind else "legit"
@@ -19,7 +19,6 @@ def generate_sequences(env, planner, data_len=100, num_fraud_seq=50):
             seq = None
 
             while not seq:
-                time.sleep(10)
                 if label == "fraud": seq, __, __, __ = planner.generate_valid_fraud_seq()
                 else: seq = planner.generate_valid_legit_seq()
 
